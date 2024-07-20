03c9aad387aa8f4851ff753bc3669a55db56af44
yeji <yezi0512@handong.ac.kr>
2024-07-20 17:19:13 +0900

    불필요한 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>예라야 안녕</h1>
+      <h2>우리 파이팅 이라는 걸 해보장구리</h2>
     </div>
   );
 }
