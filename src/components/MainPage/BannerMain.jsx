2beffd2f93a597f6692f17339f9f728869e81a39
YearaChoi <22200750@handong.ac.kr>
2024-07-23 15:25:57 +0900

    fix: 화면 크기 변경시 배너가 배경에 전부 채워지도록 수정

--- src/components/MainPage/BannerMain.jsx
+++ src/components/MainPage/BannerMain.jsx
@@ -3,11 +3,7 @@ import styled from "styled-components";
 import mainBannerImg from "../../assets/img/MainBanner.svg";
 
 function BannerMain() {
-  return (
-    <Wrapper>
-      <img src={mainBannerImg} alt="메인페이지 배너"></img>
-    </Wrapper>
-  );
+  return <Wrapper></Wrapper>;
 }
 
 export default BannerMain;
@@ -15,4 +11,9 @@ export default BannerMain;
 const Wrapper = styled.div`
   border: 3px solid green;
   height: 413px;
+
+  background-image: url(${mainBannerImg});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 `;
