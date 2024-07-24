66abbf59dddb20b9cb6cc0c06c580c2442921463
YearaChoi <22200750@handong.ac.kr>
2024-07-24 20:34:54 +0900

    style: 라이트버전 헤더 스타일 작업

--- src/components/Common/HeaderLightVer.jsx
+++ src/components/Common/HeaderLightVer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import pickleLogo from "../../assets/logo/PickleLogo.svg";
 import { useNavigate } from "react-router-dom";
+import HeaderSearchBar from "./HeaderSearchBar";
 
 function HeaderLightVer() {
   const navigate = useNavigate();
@@ -12,14 +13,19 @@ function HeaderLightVer() {
 
   return (
     <Wrapper>
-      <Logo>
-        {" "}
-        <img
-          onClick={handlePickleLogoClick}
-          src={pickleLogo}
-          alt="피클 로고"
-        ></img>
-      </Logo>
+      <Menus>
+        <Menu>홈</Menu>
+        <Menu>강의목록</Menu>
+        <Menu>마이페이지</Menu>
+        <Logo>
+          <img
+            onClick={handlePickleLogoClick}
+            src={pickleLogo}
+            alt="피클 로고"
+          ></img>
+        </Logo>
+      </Menus>
+      <HeaderSearchBar />
     </Wrapper>
   );
 }
@@ -27,15 +33,36 @@ function HeaderLightVer() {
 export default HeaderLightVer;
 
 const Wrapper = styled.div`
-  border: 1px solid #ebebeb;
   height: 98px;
   font-size: 20px;
   display: flex;
   justify-content: center;
 `;
 
+const Menus = styled.div`
+  border-bottom: 1px solid #42a8f8;
+  width: 100%;
+  margin-right: 15px;
+  display: flex;
+`;
+
+const Menu = styled.div`
+  /* border: 2px solid green; */
+  margin-top: 60px;
+  font-size: 18px;
+  color: #42a8f8;
+  cursor: pointer;
+  margin-left: 55px;
+
+  &:first-child {
+    margin-left: 244px;
+  }
+`;
+
 const Logo = styled.div`
   height: 25px;
   width: 90px;
+  margin-top: 54px;
+  margin-left: 200px;
   cursor: pointer;
 `;
---
+++ src/components/Common/HeaderSearchBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+function HeaderSearchBar() {
+  return <Wrapper>HeaderSearchBar</Wrapper>;
+}
+
+export default HeaderSearchBar;
+
+const Wrapper = styled.div`
+  border: 2px solid red;
+  width: 340px;
+`;
