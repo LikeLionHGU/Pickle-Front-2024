062f97971b80bac5b58a9ab7dacd5c087669d1cd
YearaChoi <22200750@handong.ac.kr>
2024-07-25 16:48:49 +0900

    feat: 파란 버튼 컴포넌트 생성

---
+++ src/components/Common/BlueBtn.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+function BlueBtn({ children }) {
+  return <Wrapper>{children}</Wrapper>;
+}
+
+export default BlueBtn;
+
+const Wrapper = styled.div`
+  background-color: #42a8f8;
+  border-radius: 15px;
+  color: white;
+  height: 30px;
+  width: 125px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 15px;
+  cursor: pointer;
+`;
