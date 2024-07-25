9677a536028d9d13af6d1805471f74a7dabb8cfc
YearaChoi <22200750@handong.ac.kr>
2024-07-25 16:18:23 +0900

    style: 강의 목록 페이지 스타일 작업

---
+++ src/components/LectureListPage/LectureFilterContainer.jsx
@@ -0,0 +1,313 @@
+import React, { useState, useRef, useEffect } from "react";
+import styled from "styled-components";
+import CalendarCom from "../Common/CalendarCom";
+
+function LectureFilterContainer() {
+  const [regionDropdown, setRegionDropdown] = useState(false);
+  const [sportsDropdown, setSportsDropdown] = useState(false);
+  const [subOptions, setSubOptions] = useState([]);
+  const [subDropdown, setSubDropdown] = useState(false);
+  const regionRef = useRef();
+  const sportsRef = useRef();
+
+  const regionOptions = [
+    { name: "강원", subOptions: ["강원원"] },
+    { name: "경기", subOptions: ["과천시", "의왕시", "안양시"] },
+    { name: "경남", subOptions: [] },
+    { name: "경북", subOptions: [] },
+    { name: "광주", subOptions: [] },
+    {
+      name: "대구",
+      subOptions: [
+        "군위군",
+        "남구",
+        "달서구",
+        "달성군",
+        "동구",
+        "북구",
+        "서구",
+        "수성구",
+        "중구",
+      ],
+    },
+    { name: "대전", subOptions: [] },
+    { name: "부산", subOptions: [] },
+    { name: "서울", subOptions: [] },
+    { name: "세종", subOptions: [] },
+    { name: "울산", subOptions: [] },
+    { name: "인천", subOptions: [] },
+    { name: "전남", subOptions: [] },
+    { name: "전북", subOptions: [] },
+    { name: "제주", subOptions: [] },
+    { name: "충남", subOptions: [] },
+    { name: "충북", subOptions: [] },
+  ];
+
+  const sportsOptions = [
+    { name: "헬스", subOptions: ["개인PT", "헬스"] },
+    { name: "수영", subOptions: ["실내수영", "실외수영"] },
+    {
+      name: "구기종목",
+      subOptions: [
+        "축구",
+        "야구",
+        "배구",
+        "농구",
+        "탁구",
+        "테니스",
+        "당구",
+        "골프",
+      ],
+    },
+    {
+      name: "기타",
+      subOptions: [
+        "요가",
+        "필라테스",
+        "배드민턴",
+        "복싱",
+        "태권도",
+        "펜싱",
+        "주짓수",
+        "기타",
+      ],
+    },
+  ];
+
+  const handleClickOutside = (event) => {
+    if (
+      regionRef.current &&
+      !regionRef.current.contains(event.target) &&
+      sportsRef.current &&
+      !sportsRef.current.contains(event.target)
+    ) {
+      setRegionDropdown(false);
+      setSportsDropdown(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const toggleRegionDropdown = () => {
+    setRegionDropdown(!regionDropdown);
+    setSportsDropdown(false);
+    setSubDropdown(false);
+  };
+
+  const toggleSportsDropdown = () => {
+    setSportsDropdown(!sportsDropdown);
+    setRegionDropdown(false);
+    setSubDropdown(false);
+  };
+
+  const handleOptionClick = (option) => {
+    if (option.subOptions.length > 0) {
+      setSubOptions(option.subOptions);
+      setSubDropdown(true);
+    } else {
+      setSubDropdown(false);
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Text>원하는 운동 강좌를 검색해보세요</Text>
+      <FilterContainer>
+        <FilterContent ref={regionRef}>
+          <FilterTitle onClick={toggleRegionDropdown}>지역</FilterTitle>
+          {regionDropdown && (
+            <DropdownMenu>
+              <DropdownSections>
+                <DropdownSection>
+                  {regionOptions
+                    .slice(0, Math.ceil(regionOptions.length / 2))
+                    .map((option) => (
+                      <DropdownItem
+                        key={option.name}
+                        onClick={() => handleOptionClick(option)}
+                      >
+                        {option.name}
+                      </DropdownItem>
+                    ))}
+                </DropdownSection>
+                <DropdownSection>
+                  {regionOptions
+                    .slice(Math.ceil(regionOptions.length / 2))
+                    .map((option) => (
+                      <DropdownItem
+                        key={option.name}
+                        onClick={() => handleOptionClick(option)}
+                      >
+                        {option.name}
+                      </DropdownItem>
+                    ))}
+                </DropdownSection>
+                <SubDropdownSection>
+                  {subDropdown &&
+                    subOptions.map((subOption) => (
+                      <DropdownItem key={subOption}>{subOption}</DropdownItem>
+                    ))}
+                </SubDropdownSection>
+              </DropdownSections>
+            </DropdownMenu>
+          )}
+        </FilterContent>
+        <FilterContent ref={sportsRef}>
+          <FilterTitle onClick={toggleSportsDropdown}>운동종목</FilterTitle>
+          {sportsDropdown && (
+            <DropdownMenu>
+              <DropdownSections>
+                <DropdownSection>
+                  {sportsOptions
+                    .slice(0, Math.ceil(sportsOptions.length / 2))
+                    .map((option) => (
+                      <DropdownItem
+                        key={option.name}
+                        onClick={() => handleOptionClick(option)}
+                      >
+                        {option.name}
+                      </DropdownItem>
+                    ))}
+                </DropdownSection>
+                <DropdownSection>
+                  {sportsOptions
+                    .slice(Math.ceil(sportsOptions.length / 2))
+                    .map((option) => (
+                      <DropdownItem
+                        key={option.name}
+                        onClick={() => handleOptionClick(option)}
+                      >
+                        {option.name}
+                      </DropdownItem>
+                    ))}
+                </DropdownSection>
+                <SubDropdownSection>
+                  {subDropdown &&
+                    subOptions.map((subOption) => (
+                      <DropdownItem key={subOption}>{subOption}</DropdownItem>
+                    ))}
+                </SubDropdownSection>
+              </DropdownSections>
+            </DropdownMenu>
+          )}
+        </FilterContent>
+        <FilterContent>
+          <FilterTitle>장애유형</FilterTitle>
+        </FilterContent>
+        <FilterContent>
+          <FilterTitle>날짜</FilterTitle>
+        </FilterContent>
+        <FilterContent>
+          <FilterTitle>가격</FilterTitle>
+        </FilterContent>
+        <SearchBtn>
+          <SearchBtnTitle>검색</SearchBtnTitle>
+        </SearchBtn>
+      </FilterContainer>
+      {/* <CalendarCom /> */}
+    </Wrapper>
+  );
+}
+
+export default LectureFilterContainer;
+
+const Wrapper = styled.div`
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+  width: 100%;
+  height: 153px;
+  background-color: white;
+  border-radius: 10px;
+`;
+
+const Text = styled.div`
+  margin-top: 29px;
+  margin-left: 41px;
+  margin-bottom: 45px;
+  padding-top: 32px;
+  font-size: 18px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  margin-left: 30px;
+  margin-bottom: 28px;
+`;
+
+const FilterContent = styled.div`
+  margin-right: 20px;
+  width: 139px;
+  height: 29px;
+  border-radius: 27.5px;
+  background-color: #e8e8e8;
+`;
+
+const FilterTitle = styled.div`
+  margin-top: 6px;
+  margin-left: 14px;
+  font-size: 15px;
+  color: #747474;
+  cursor: pointer;
+`;
+
+const DropdownMenu = styled.div`
+  padding-top: 27px;
+  padding-left: 23px;
+  margin-top: 5px;
+  width: 422px;
+  height: 342px;
+  background-color: white;
+  border: 1px solid #d5d5d5;
+  border-radius: 10px;
+  z-index: 1;
+`;
+
+const DropdownSections = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  height: 100%;
+`;
+
+const DropdownSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-right: 10px;
+  overflow-y: auto;
+`;
+
+const SubDropdownSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 10px;
+  overflow-y: auto;
+`;
+
+const DropdownItem = styled.div`
+  margin-bottom: 15px;
+  cursor: pointer;
+  color: ${(props) => (props.isSelected ? "#49abf9" : "black")};
+  &:hover {
+    color: #49abf9;
+  }
+`;
+
+const SearchBtn = styled.div`
+  width: 126px;
+  height: 29px;
+  border-radius: 14.5px;
+  font-size: 15px;
+  background-color: #42a8f8;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+const SearchBtnTitle = styled.div`
+  color: white;
+`;
