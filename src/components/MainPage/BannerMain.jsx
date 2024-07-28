6d51ce1eb50b7f42925e225bb20cf8a482e3935f
YearaChoi <22200750@handong.ac.kr>
2024-07-28 14:43:56 +0900

    add: 메인페이지 배너 적용

--- src/components/MainPage/BannerMain.jsx
+++ src/components/MainPage/BannerMain.jsx
@@ -3,17 +3,26 @@ import styled from "styled-components";
 import mainBannerImg from "../../assets/img/MainBanner.svg";
 
 function BannerMain() {
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <BannerBg></BannerBg>
+    </Wrapper>
+  );
 }
 
 export default BannerMain;
 
 const Wrapper = styled.div`
-  border: 3px solid green;
+  /* border: 3px solid green; */
   height: 413px;
+  overflow: hidden;
+`;
 
+const BannerBg = styled.div`
   background-image: url(${mainBannerImg});
+  width: 108%;
+  height: 118%;
   background-size: cover;
-  background-position: center;
+  background-position: left;
   background-repeat: no-repeat;
 `;
