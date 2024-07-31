922c916d621ad4e27f9f9f6d56da79e9d322f1c1
YearaChoi <22200750@handong.ac.kr>
2024-07-31 13:47:07 +0900

    delete: 메인 페이지 검색 컴포넌트 중복 삭제

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,13 +3,11 @@ import HeaderMain from "../components/MainPage/HeaderMain";
 import BannerMain from "../components/MainPage/BannerMain";
 import ContentMain from "../components/MainPage/ContentMain";
 import FilterContainerMain from "../components/MainPage/FilterContainerMain";
-import HeaderSearchBar from "../components/Common/HeaderSearchBar";
 
 export default function MainPage() {
   return (
     <>
       <HeaderMain />
-      <HeaderSearchBar />
       <FilterContainerMain />
       <BannerMain />
       <ContentMain />
