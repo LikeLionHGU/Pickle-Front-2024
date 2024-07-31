7495742f64558567dd723663fe793fcc5a794717
YearaChoi <22200750@handong.ac.kr>
2024-07-31 13:41:40 +0900

    sytle: 로그인 모달 스타일 작업

--- src/components/MainPage/LoginModal.jsx
+++ src/components/MainPage/LoginModal.jsx
@@ -1,12 +1,35 @@
 import React from "react";
 import styled from "styled-components";
+import exitBtn from "../../assets/img/exit.svg";
+import googleLoginBtn from "../../assets/img/googleLoginBtn.svg";
+import kakaoLoginBtn from "../../assets/img/kakaoLoginBtn.svg";
 
 function LoginModal({ toggleModal }) {
   return (
     <Modal>
       <Overlay onClick={toggleModal} />
       <ModalContent>
-        <ModalWrapper></ModalWrapper>
+        <ModalWrapper>
+          <Top>
+            <img
+              onClick={toggleModal}
+              src={exitBtn}
+              alt="모달 닫기 아이콘"
+            ></img>
+          </Top>
+          <Login>로그인</Login>
+          <Content>
+            <GoogleLoginBtn
+              src={googleLoginBtn}
+              alt="구글 로그인 버튼"
+            ></GoogleLoginBtn>
+            <KakaoLoginBtn
+              src={kakaoLoginBtn}
+              alt="카카오 로그인 버튼"
+            ></KakaoLoginBtn>
+          </Content>
+          <SignIn>회원가입</SignIn>
+        </ModalWrapper>
       </ModalContent>
     </Modal>
   );
@@ -47,9 +70,52 @@ const ModalContent = styled.div`
 `;
 
 const ModalWrapper = styled.div`
-  border: 1px solid red;
+  /* border: 1px solid red; */
   width: 285px;
   height: 620px;
   margin: auto;
   margin-top: 20px;
+`;
+
+const Top = styled.div`
+  /* border: 1px solid red; */
+  text-align: right;
+
+  img {
+    height: 15px;
+    cursor: pointer;
+  }
+`;
+
+const Login = styled.div`
+  margin-top: 15px;
+  text-align: center;
+  font-size: 24px;
+  font-family: "PretendardSemiBold";
+`;
+
+const Content = styled.div`
+  margin-top: 37px;
+`;
+
+const GoogleLoginBtn = styled.img`
+  width: 100%;
+  height: 38px;
+  cursor: pointer;
+`;
+
+const KakaoLoginBtn = styled.img`
+  margin-top: 10px;
+  width: 100%;
+  height: 38px;
+  cursor: pointer;
+`;
+
+const SignIn = styled.div`
+  color: #4a4a4a;
+  font-size: 14px;
+  text-align: end;
+  margin-top: 29px;
+  text-decoration: underline;
+  cursor: pointer;
 `;
