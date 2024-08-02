cd0d55fbcf6aba4611de4e2c1ecb7637be72fddb
yeji <yezi0512@handong.ac.kr>
2024-08-02 17:31:33 +0900

    feat: 지도 api key 재설정

--- src/components/MainPage/ContentMain.jsx
+++ src/components/MainPage/ContentMain.jsx
@@ -59,7 +59,7 @@ function ContentMain() {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 2,
+    slidesToShow: 4,
     slidesToScroll: 2,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
